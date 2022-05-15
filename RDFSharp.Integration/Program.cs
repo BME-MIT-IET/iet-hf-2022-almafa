b29720ea23a89d6a37d5b1b3b86241a314d769e5
Zsombor Szommer <zsombor.szommer@protonmail.com>
2022-05-15 17:56:22 +0200

    #5 First working example

--- RDFSharp.Integration/Program.cs
+++ RDFSharp.Integration/Program.cs
@@ -1,3 +1,23 @@
-﻿using RDFSharp.Query;
+﻿using RDFSharp.Model;
+using RDFSharp.Query;
 
-RDFSelectQuery a;
+// CREATE SELECT QUERY
+var selectQuery =
+    new RDFSelectQuery().AddPatternGroup(
+        new RDFPatternGroup("pg1").AddPattern(
+            new RDFPattern(
+                new RDFVariable("?actor"),
+                RDFVocabulary.RDFS.LABEL,
+                new RDFPlainLiteral("Rembrandt", "en")
+            )
+        )
+    );
+
+// CREATE SPARQL ENDPOINTS
+var dbPediaEndpnt = new RDFSPARQLEndpoint(new Uri("https://dbpedia.org/sparql"));
+
+// APPLY SELECT QUERY TO SPARQL ENDPOINT
+var selectQueryResult = await selectQuery.ApplyToSPARQLEndpointAsync(dbPediaEndpnt);
+
+// SHOW RESULTS
+selectQueryResult.ToSparqlXmlResult(Console.OpenStandardOutput());
