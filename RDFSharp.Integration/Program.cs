bbe08cebd9c37861ee0c9d3490fbdc18e523d0d4
Zsombor Szommer <zsombor.szommer@protonmail.com>
2022-05-15 19:56:50 +0200

    I am genius!

--- RDFSharp.Integration/Program.cs
+++ RDFSharp.Integration/Program.cs
@@ -1,17 +1,46 @@
 ﻿using RDFSharp.Model;
 using RDFSharp.Query;
 
+/*
+
+SELECT ?description
+WHERE
+{
+    ?actor rdfs:label "Rembrandt"@en .
+    ?actor dbo:abstract ?description
+    FILTER ( LANG ( ?description ) = 'en' )
+}
+LIMIT 1
+
+*/
+
 // CREATE SELECT QUERY
+// Create variables
+var actor = new RDFVariable("?actor");
+var description = new RDFVariable("?description");
+// Compose query
 var selectQuery =
     new RDFSelectQuery().AddPatternGroup(
         new RDFPatternGroup("pg1").AddPattern(
             new RDFPattern(
-                new RDFVariable("?actor"),
+                actor,
                 RDFVocabulary.RDFS.LABEL,
                 new RDFPlainLiteral("Rembrandt", "en")
             )
         )
-    );
+        .AddPattern(
+            new RDFPattern(
+                actor,
+                new RDFResource(string.Concat("http://dbpedia.org/ontology/", "abstract")),
+                description
+            )
+        )
+        .AddFilter(
+            new RDFLangMatchesFilter(description, "en")
+        )
+    )
+    .AddModifier(new RDFLimitModifier(1))
+    .AddProjectionVariable(description);
 
 // CREATE SPARQL ENDPOINTS
 var dbPediaEndpnt = new RDFSPARQLEndpoint(new Uri("https://dbpedia.org/sparql"));
@@ -19,5 +48,6 @@ var dbPediaEndpnt = new RDFSPARQLEndpoint(new Uri("https://dbpedia.org/sparql"))
 // APPLY SELECT QUERY TO SPARQL ENDPOINT
 var selectQueryResult = await selectQuery.ApplyToSPARQLEndpointAsync(dbPediaEndpnt);
 
-// SHOW RESULTS
-selectQueryResult.ToSparqlXmlResult(Console.OpenStandardOutput());
+var about = selectQueryResult.SelectResults.Rows[0].ItemArray[0]?.ToString();
+
+Console.WriteLine(about);
