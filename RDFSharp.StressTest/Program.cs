6f82ff8cfc0226ce0eed1e55307d48b687ce291c
DESKTOP-10IK837\foldv <foldvariadamdavid@gmail.com>
2022-05-19 11:53:22 +0200

    Az egyik midnig sokkal gyorsabb mint a másik

---
+++ RDFSharp.StressTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using RDFSharp.Model;
+using RDFSharp.Query;
+using RDFSharp.StressTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDFSharp.ManualTest
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TestModels asd = new TestModels();
+
+            asd.Run();
+
+            Console.WriteLine("All tests are finished.");
+        }
+    }
+}
---
+++ RDFSharp.StressTest/TestModels.cs
@@ -0,0 +1,107 @@
+﻿using RDFSharp.Model;
+using System.Diagnostics;
+using RDFSharp.Query;
+
+namespace RDFSharp.StressTest
+{
+    public class TestModels
+    {
+        private static RDFGraph Graph;
+
+        public void Run()
+        {
+            Console.WriteLine("Started testing loading graphs.");
+            allActorQuery();
+            rembrandtQuery();
+            Console.WriteLine("Finished testing loading graphs.");
+        }
+
+        public void allActorQuery()
+        {
+            Console.WriteLine("All Actor Query started\n");
+            DateTime start;
+            DateTime end;
+            TimeSpan total_time = TimeSpan.Zero;
+            Console.WriteLine(total_time);
+
+            Process myProcess = Process.GetCurrentProcess();
+            Console.WriteLine($"Start processor time            : {myProcess.TotalProcessorTime}");
+            Console.WriteLine($"Start physical memory usage     : {myProcess.WorkingSet64}");
+
+            var actor = new RDFVariable("?actor");
+
+            var selectQuery =
+                new RDFSelectQuery().AddPatternGroup(
+                    new RDFPatternGroup("pg1").AddPattern(
+                        new RDFPattern(
+                            actor,
+                            RDFVocabulary.RDFS.DATATYPE,
+                            new RDFPlainLiteral("ecrm:E39_Actor")
+                        )
+                    )
+                );
+
+            start = DateTime.Now;
+            var queryResult = selectQuery.ApplyToGraph(Graph);
+            end = DateTime.Now;
+
+            total_time = (end - start);
+
+            Console.WriteLine($"Total loading time ms           : {total_time.TotalMilliseconds} ms");
+            Console.WriteLine($"End processor time              : {myProcess.TotalProcessorTime}");
+            Console.WriteLine($"End physical memory usage       : {myProcess.WorkingSet64} \n");
+            Console.WriteLine("All Actor Query ended\n");
+        }
+
+        public void rembrandtQuery()
+        {
+            Console.WriteLine("RembrandtQuery started\n");
+            DateTime start;
+            DateTime end;
+            TimeSpan total_time = TimeSpan.Zero;
+            Console.WriteLine(total_time);
+
+            Process myProcess = Process.GetCurrentProcess();
+            Console.WriteLine($"Start processor time            : {myProcess.TotalProcessorTime}");
+            Console.WriteLine($"Start physical memory usage     : {myProcess.WorkingSet64}");
+
+            var actor = new RDFVariable("?actor");
+            var description = new RDFVariable("?description");
+
+            var selectQuery =
+                new RDFSelectQuery().AddPatternGroup(
+                    new RDFPatternGroup("pg1").AddPattern(
+                        new RDFPattern(
+                            actor,
+                            RDFVocabulary.RDFS.LABEL,
+                            new RDFPlainLiteral("Rembrandt", "en")
+                        )
+                    )
+                    .AddPattern(
+                        new RDFPattern(
+                            actor,
+                            new RDFResource(string.Concat("http://dbpedia.org/ontology/", "abstract")),
+                            description
+                        )
+                    )
+                    .AddFilter(
+                        new RDFLangMatchesFilter(description, "en")
+                    )
+                )
+                .AddModifier(new RDFLimitModifier(1))
+                .AddProjectionVariable(description);
+
+            
+            start = DateTime.Now;
+            var queryResult = selectQuery.ApplyToGraph(Graph);
+            end = DateTime.Now;
+
+            total_time = (end - start);
+
+            Console.WriteLine($"Total loading time ms           : {total_time.TotalMilliseconds} ms");
+            Console.WriteLine($"End processor time              : {myProcess.TotalProcessorTime}");
+            Console.WriteLine($"End physical memory usage       : {myProcess.WorkingSet64} \n");
+            Console.WriteLine("RembrandtQuery ended\n");
+        }
+    }
+}
